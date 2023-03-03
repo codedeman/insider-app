c30bcf776de178eb116046feeb6d6ff45f1d154e
Kien Pham <kienluproductions@gmail.com>
2023-03-04 07:16:58 +0800

    firt commit -m

---
+++ InsiderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InsiderApp
+//
+//  Created by Kevin on 3/3/23.
+//
+
+import UIKit
+import Home
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let home = Hom
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
